e00125c4a95bc9b0acd7a338c0101c9b382a792c
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-16 15:25:51 -0700

    fix_syntax_error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,8 +18,8 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else 
-		printf("%d id negative\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }

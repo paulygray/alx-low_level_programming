536bfba747b6f50912de84e2ef99241580972035
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-16 21:18:06 -0700

    alphabet_tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (bet = 0; bet < 26; bet++)
 	{
-		putchar('a' + 1);
+		putchar('a' + bet);
 	}
 
 	for (bet = 0; bet < 26; bet++)
 	{
-		putchar('A' + 1);
+		putchar('A' + bet);
 	}
 
 	putchar('\n');

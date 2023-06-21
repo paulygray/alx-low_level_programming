4f918dadaf50d0d221e32e47fdf694ccdb89b7cb
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-20 20:45:22 -0700

    task13-nature_made_the_natural_numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print sum of all the multiples of 3 or 5 below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			m += n;
+	}
+	printf("%d\n", b);
+
+	return (0);
+}

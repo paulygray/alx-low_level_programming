1a8cdc74d0e6ded34a1784347dfed7435b3215e7
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-20 19:28:14 -0700

    task10-a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/**
+ * add - Function
+ * 
+ * Description: Add two integers and return result
+ *
+ * @a: Integer param value
+ * @b: next integer param value
+ * 
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}

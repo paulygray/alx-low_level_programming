6e01460f2a08a3499d822eb651c6e156dc16d585
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-20 19:31:28 -0700

    fix trailing whitespace

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 /**
  * add - Function
- * 
+ *
  * Description: Add two integers and return result
  *
  * @a: Integer param value
  * @b: next integer param value
- * 
+ *
  * Return: sum
  */
 int add(int a, int b)

ad9ecede3b414d10734fc0fd4e4b42d6e8780db8
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-06-20 20:29:09 -0700

    task12-the_world_looks_like_a_multiplication_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_times_table - Function definition
+ *
+ * Description: Print n times-table starting with 0
+ *
+ * @n: Integer param value
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n >= 0 && n < 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+
+				if (y == 0)
+					_putchar('0');
+				else if (z < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z % 10 + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) % 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+				else if (z > 99 && z < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(z / 100 + '0');
+					_putchar((z / 10) % 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}

0ab1ecdf933627c4a2f9a73c598659f5b02dfe2f
Paul Ogbazi <ebubeogbazi@gmail.com>
2023-07-04 02:03:59 -0700

    fix logic

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,10 +13,10 @@ void print_diagsums(int *a, int size)
 	long int z1 = 0, z2 = 0;
 
 	for (n = 0; n < size; n++)
-	{
-		z1 = z1 + *(a + n * size + n);
-		z2 = z2 + *(a + n * size - n);
-	}
+		z1 += a[n * size + n];
+
+	for (n = size - 1; n >= 0; n--)
+		z2 += a[n * size + (size - n - 1)];
 
 	printf("%ld, %ld\n", z1, z2);
 }
